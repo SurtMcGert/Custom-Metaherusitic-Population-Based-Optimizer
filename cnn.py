04cbadabbe14b98bc255537ac2734339234aaded
jkrrrr <102986583+jkrrrr@users.noreply.github.com>
2023-11-25 12:41:37 +0000

    updated cnn.py for Grey Wolf optimizer

--- cnn.py
+++ cnn.py
@@ -9,6 +9,9 @@ from torch import flatten
 
 class CNN(Module):
     def __init__(self, numChannels, classes):
+        super().__init__()
+        self.input = None
+        self.y = None
         self.classes = classes
         # call the parent constructor
         super(CNN, self).__init__()
@@ -30,6 +33,7 @@ class CNN(Module):
         self.logSoftmax = LogSoftmax(dim=1)
 
     def forward(self, x):
+        self.input = x
         # pass the input through our first set of CONV => RELU =>
         # POOL layers
         x = self.conv1(x)
@@ -51,6 +55,63 @@ class CNN(Module):
         output = self.logSoftmax(x)
         # return the output predictions
         return output
+
+    def reInitializeFinalLayer(self):
+        # freeze all layers except the last and reset its parameters
+        self.last_layer = Linear(in_features=500, out_features=self.classes)
+        for name, layer in self.named_parameters():
+            if 'last' in name:
+                layer.requires_grad = True
+            else:
+                layer.requires_grad = False
+
+class VGG13(Module):
+    def __init__(self, numChannels, classes):
+        super(VGG13, self).__init__()
+        self.features = Sequential(
+            Conv2d(numChannels, 64, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            Conv2d(64, 64, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+            Conv2d(64, 128, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            Conv2d(128, 128, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+            Conv2d(128, 256, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            Conv2d(256, 256, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+            Conv2d(256, 512, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            Conv2d(512, 512, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+            Conv2d(512, 512, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            Conv2d(512, 512, kernel_size=3, padding=1),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.avgpool = AdaptiveAvgPool2d((7, 7))  # Adaptive pooling to (7, 7)
+        self.classifier = Sequential(
+            Linear(512 * 7 * 7, 4096),
+            ReLU(inplace=True),
+            Dropout(),
+            Linear(4096, 4096),
+            ReLU(inplace=True),
+            Dropout(),
+            Linear(4096, classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = flatten(x, 1)  # Flatten the tensor
+        x = self.classifier(x)
+        return x
 
     def reInitializeFinalLayer(self):
         # freeze all layers except the last and reset its parameters
